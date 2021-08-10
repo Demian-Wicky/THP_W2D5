8459a66b4e0e906eb12bcbccc2144e4138d4631c
Demian-Wicky <demian.wicky@gmail.com>
2021-08-10 17:51:41 +0200

    kata_03.rb OK

--- lib/kata_03.rb
+++ lib/kata_03.rb
@@ -1,7 +1,14 @@
 def get_middle(s)
-  
+  if s.length.odd? # si longueur est impair
+    idx = s.length/2 #index du caractère à conserver
+    s[idx]
+  else # si longueur est pair
+    idx1 = s.length/2-1 #index du premier caractère à conserver
+    idx2 = s.length/2 #index du second caractère à conserver
+    s[idx1]+s[idx2]
+  end
 end
 
-s = "testing"
+s = "middle"
 
 puts get_middle(s)
